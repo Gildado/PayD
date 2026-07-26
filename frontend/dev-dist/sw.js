ec46610c3e8b088c62f02f1c814444b897c6dbca
petnacode <peternnadi1999@gmail.com>
2026-07-26 20:43:29 +0100

    fix merge conflict

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-0791036e'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.bq01norb21k"
+    "revision": "0.opo123r8gh"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
